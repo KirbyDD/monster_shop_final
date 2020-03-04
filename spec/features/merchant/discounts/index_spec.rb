fff796edc79f653d6d1e967325baf5001cd3a71c
DeMarcus Kirby <tkd024@gmail.com>
2020-03-04 00:32:03 -0700

    Add test for index feature test

---
+++ spec/features/merchant/discounts/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "As a merchant", type: :feature do
+  before :each do
+    @bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+    @user_1 = @bike_shop.users.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 1)
+    @twenty_perc = @bike_shop.discounts.create!(percentage: 20, quantity: 5)
+    @ten_perc = @bike_shop.discounts.create!(percentage: 10, quantity: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+  end
+  it 'should show all discounts' do
+    visit '/merchant/discounts'
+
+    within "#discount-#{@twenty_perc.id}" do
+      expect(page).to have_content("Discount Percentage: 20%")
+      expect(page).to have_content("Item Quantity Needed: 5")
+    end
+
+    within "#discount-#{@ten_perc.id}" do
+      expect(page).to have_content("Discount Percentage: 10%")
+      expect(page).to have_content("Item Quantity Needed: 2")
+    end
+  end
+end
